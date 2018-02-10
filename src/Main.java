b3fd704397200a2ec542a094988634c760fe6dd3
VladoGoIt <dzhuhalyk@gmail.com>
2018-02-10 22:35:32 +0200

    test

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        double a;
+        double b;
+        double c;
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter firs side");
+            a = scanner.nextDouble();
+
+            System.out.println("Enter second side");
+            b = scanner.nextDouble();
+
+            System.out.println("Enter third side");
+            c = scanner.nextDouble();
+
+            double area = triangleArea(a, b, c);
+
+            System.out.println(area);
+            System.out.println("______________________");
+            System.out.println();
+
+        }
+
+
+
+
+
+
+    }
+
+    public static double triangleArea(double a, double b, double c){
+        double p = (a+b+c)/2;
+        double s = Math.sqrt((p*(p-a)*(p-b)*(p-c)));
+        return s;
+    }
+
+}
